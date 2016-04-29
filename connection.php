cbe12da509d9cf69b6c4c7a338f9e885d6c4b24e
1312897 <fredakanapo@yahoo.co.uk>
2016-04-29 21:18:58 +0100

    thanks

---
+++ connection.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1312897
+ * Date: 29/04/2016
+ * Time: 21:14*/
+
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME','bceed72e8f396d');
+define('DB_PASSWORD', '1e3ecefa');
+define('DB_DATABASE', 'CMM007ALDB-1312897');
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+else{
+echo "success connect to db";
+}
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 echo"Wale is bad boy he is really helpful";
+/*
+ *ap-cdbr-azure-east-c.cloudapp.net :severname
+ *
+ *bceed72e8f396d : username
+ *
+ *1e3ecefa : password
+ *
+ * CMM007ALDB-1312897
+*/
 ?>
