72d4dcb62d9d96cc4206de7baa72d0d0ce37cd41
1312897 <fredakanapo@yahoo.co.uk>
2016-05-04 12:16:13 +0100

    exam

---
+++ partthree/add.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link type="text/css" rel="stylesheet" href="assets/style.css" />
+    <link type="text/css" rel="stylesheet" href="assets/styleform.css" />
+    <link type="text/css" rel="stylesheet" href="../../CMM07AL-1312897/partone/assets/unsemantic-grid-responsive-tablet.css" />
+    <link href='https://fonts.googleapis.com/css?family=Roboto:700,300,400' rel='stylesheet' type='text/css'>
+    <title>myBlog</title>
+</head>
+<body>
+<header>
+    <h1>my Blog</h1>
+    <p>because the internet needs to know what I think</p>
+</header>
+<nav>
+    <ul>
+        <li><a href="blog.php">All Blog Items</a> </li>
+        <li><a href="blog.php?category=Work">Work Blog Items</a> </li>
+        <li><a href="blog.php?category=University">University Blog Items</a> </li>
+        <li><a href="blog.php?category=Family">Family Items</a> </li>
+        <li><a href="add.php">Insert Bugs</a> </li>
+    </ul>
+</nav>
+<main>
+    <?
+    include("connection.php");
+if($_SERVER['REQUEST_METHOD']==='GET'){
+    echo
+
+    "<form action=\"addblog.php\" method=\"post\">
+
+                    <ul class=\"form-style-1\">
+
+        <li>
+            <label>Enter Title<span class=\"required\">*</span>
+                <input type=\"text\" name=\"title\" class=\"field-long\" />
+            </label>
+        </li>
+
+        <li>
+            <label>Enter Summary<span class=\"required\">*</span>
+                <textarea name=\"summary\" id=\"field5\" class=\"field-long field-textarea\"></textarea>
+            </label>
+        </li>
+
+        <li>
+            <label>Category
+                <select name=\"category\" class=\"field-select\" required>
+                    <option value=\"\" selected disabled>Select</option>
+                    <option value=\"Work\">Android</option>
+                    <option value=\"University\">iOS</option>
+                    <option value=\"Family\">Windows</option>
+                </select>
+            </label>
+        </li>
+        <li>
+            <label>Submitted By:<span class=\"required\">*</span>
+                <input type=\"text\" name=\"submitter\" class=\"field-long\" />
+            </label>
+        </li>
+
+        <li>
+            <input type=\"submit\" value=\"Submit\" />
+        </li>
+    </ul>
+    </form>";
+}
+  elseif($_SERVER['REQUEST_METHOD']==='POST'){
+            $entryTitle = $_POST['title'];
+            $summary = $_POST['summary'];
+            $category = $_POST['category'];
+            $submitter = $_POST['submitter'];
+            $sql = "INSERT INTO blogView(entryTitle, entrySummary, category, submitter)
+                    VALUES ('$entryTitle', '$summary', '$category','$submitter')";
+            if($db->query($sql)){
+                header("location:blog.php");
+            }
+            else{
+                echo"error ".$sql;
+            }
+        }
+        else{
+            header("location:index.php");
+        }
+        ?>
+            ?>
+
+
+</main>
+<footer>
+    <p>Designed by [Fred Antwi-Boateng],[2016]</p>
+</footer>
+</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1312897
+ * Date: 04/05/2016
+ * Time: 11:19
+// */
+?>
---
+++ partthree/blog.php
@@ -0,0 +1,74 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link type="text/css" rel="stylesheet" href="assets/style.css" />
+    <link type="text/css" rel="stylesheet" href="../../CMM07AL-1312897/partone/assets/unsemantic-grid-responsive-tablet.css" />
+    <link href='https://fonts.googleapis.com/css?family=Roboto:700,300,400' rel='stylesheet' type='text/css'>
+    <title>myBlog</title>
+</head>
+<body>
+<header>
+    <h1>my Blog</h1>
+    <p>because the internet needs to know what I think</p>
+    <?php
+    if(isset($_GET['category'])) {
+        $category = $_GET['category'];
+        $sql = "SELECT * FROM blogView WHERE category = '$category'";
+        $result = $db->query($sql);
+        $row = $result->fetch_array();
+        $category = $row['category'];
+        echo "<p>Keeping track of all the pesky little bugs<br>
+            You are in the {$category} category
+            </p>";
+    }
+    else{
+        echo"<p>Keeping track of all the pesky little bugs<br>
+            You are looking all the bugs in our database
+            </p>";
+    }
+    ?>
+</header>
+<nav>
+    <ul>
+        <li><a href="blog.php">All Blog Items</a> </li>
+        <li><a href="blog.php?category=Work">Work Blog Items</a> </li>
+        <li><a href="blog.php?category=University">University Blog Items</a> </li>
+        <li><a href="blog.php?category=Family">Family Items</a> </li>
+        <li><a href="add.php">Insert Bugs</a> </li>
+    </ul>
+</nav>
+<main>
+<?php
+
+if(isset($_GET['category'])){
+    $category = $_GET['category'];
+    $sql = "SELECT * FROM blogView WHERE category = '$category'";
+}
+else{
+    echo "I am here";
+    $sql = "SELECT * FROM blogView";
+}
+
+$result=$db->query($sql);
+while($row = $result->fetch_array())
+{
+    $title = $row['entryTitle'];
+    $category = $row['category'];
+    $summary = $row['entrySummary'];
+    $submitter = $row['submitter'];
+    echo "<article>
+                  <p>{$title} by {$submittermitter}</p>
+                  <p>{$category}</p>
+                  <p>{$summary}</p>
+                  </article>";
+
+    echo '<hr>';
+
+}
+$db->close();
+?>
+
+</section>
+</main>
